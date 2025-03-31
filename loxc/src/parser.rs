db67cf0f26d30c98bf2e8dcfcf7247173ee75dc2
cool-tea <943801990@qq.com>
2025-03-31 22:03:28 +0800

    Add visitor generator

---
+++ loxc/src/parser.rs
@@ -0,0 +1,26 @@
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "lox.pest"]
+pub struct LoxParser;
+
+#[cfg(test)]
+mod tests {
+    use pest::{Parser, iterators::Pairs};
+
+    use super::*;
+
+    fn print_recur(tree: Pairs<'_, Rule>) {
+        for node in tree {
+            println!("{:?}", node);
+            print_recur(node.into_inner());
+        }
+    }
+
+    #[test]
+    fn parse_string() {
+        let lox_str = "\"hello world!\"";
+        let tree = LoxParser::parse(Rule::String, lox_str).expect("failed to parse string");
+        print_recur(tree);
+    }
+}
