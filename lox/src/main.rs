af00e832123f79caafd30eb27d99bbc108532abe
cool-tea <943801990@qq.com>
2025-04-01 20:04:02 +0800

    Add basic framework

--- lox/src/main.rs
+++ lox/src/main.rs
@@ -1,3 +1,21 @@
+use std::{cmp, env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.len().cmp(&2) {
+        cmp::Ordering::Less => process::exit(run_repl()),
+        cmp::Ordering::Equal => process::exit(run_script(&args[1])),
+        cmp::Ordering::Greater => {
+            eprintln!("Usage: lox [SCRIPT]");
+            process::exit(64);
+        }
+    }
+}
+
+fn run_repl() -> i32 {
+    0
+}
+
+fn run_script(script: &str) -> i32 {
+    0
 }
