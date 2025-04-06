9f5e61490aaeb0aedc62dfa4f8d074c28d436876
cool-tea <943801990@qq.com>
2025-04-06 10:07:48 +0800

    Add ast

---
+++ loxc/src/ast.rs
@@ -0,0 +1,146 @@
+#[derive(Debug, Clone)]
+pub struct Program {
+    pub decls: Vec<Box<Decl>>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Decl {
+    Class(ClassDecl),
+    Fun(FunDecl),
+    Var(VarDecl),
+    Stmt(Stmt),
+}
+
+#[derive(Debug, Clone)]
+pub struct ClassDecl {
+    pub name: String,
+    pub superclass: Option<String>,
+    pub methods: Vec<Box<Func>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct FunDecl {
+    pub func: Box<Func>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Func {
+    pub name: String,
+    pub params: Vec<String>,
+    pub body: Box<Block>,
+}
+
+#[derive(Debug, Clone)]
+pub struct VarDecl {
+    pub name: String,
+    pub init: Option<Box<Expr>>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Stmt {
+    Expr(ExprStmt),
+    For(ForStmt),
+    If(IfStmt),
+    Print(PrintStmt),
+    Return(ReturnStmt),
+    While(WhileStmt),
+    Block(Block),
+}
+
+#[derive(Debug, Clone)]
+pub struct ExprStmt {
+    pub expr: Box<Expr>,
+}
+
+#[derive(Debug, Clone)]
+pub enum ForInitializer {
+    VarDecl(VarDecl),
+    ExprStmt(ExprStmt),
+}
+
+#[derive(Debug, Clone)]
+pub struct ForStmt {
+    pub init: Option<Box<ForInitializer>>,
+    pub cond: Option<Box<Expr>>,
+    pub post: Option<Box<Expr>>,
+    pub body: Box<Stmt>,
+}
+
+#[derive(Debug, Clone)]
+pub struct IfStmt {
+    pub cond: Box<Expr>,
+    pub then_body: Box<Stmt>,
+    pub else_body: Option<Box<Stmt>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct PrintStmt {
+    pub expr: Box<Expr>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ReturnStmt {
+    pub expr: Option<Box<Expr>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct WhileStmt {
+    pub cond: Box<Expr>,
+    pub body: Box<Stmt>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Block {
+    pub decls: Vec<Box<Decl>>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Number(f64),
+    String(String),
+    Bool(bool),
+    Nil,
+    Variable(String),
+    This,
+    Super,
+    Binary(BinaryExpr),
+    Unary(UnaryExpr),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Operator {
+    /* Binary operator */
+    Assign,
+    Or,
+    And,
+    Ne,
+    Eq,
+    Lt,
+    Gt,
+    Le,
+    Ge,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Visit, // obj.member
+
+    /* Unary operator */
+    Not,
+    Minus,
+    Invoke, // func()
+    Group,  // (expr)
+}
+
+#[derive(Debug, Clone)]
+pub struct BinaryExpr {
+    pub op: Operator,
+    pub lhs: Box<Expr>,
+    pub rhs: Box<Expr>,
+}
+
+#[derive(Debug, Clone)]
+pub struct UnaryExpr {
+    pub op: Operator,
+    pub expr: Box<Expr>,
+}
